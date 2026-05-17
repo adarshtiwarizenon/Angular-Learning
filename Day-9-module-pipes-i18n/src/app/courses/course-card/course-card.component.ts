a3aecaa1a3595c92f3e204ae00e5bb6117f14e84
Adarsh Tiwari <atiwari@zenon.ai>
2026-05-17 19:42:45 +0530

    Day-10 Angular

---
+++ Day-9-module-pipes-i18n/src/app/courses/course-card/course-card.component.ts
@@ -0,0 +1,54 @@
+import {
+  AfterContentInit,
+  AfterViewInit,
+  Attribute,
+  ChangeDetectionStrategy,
+  Component,
+  ContentChildren,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnInit,
+  OnDestroy,
+  Output,
+  QueryList,
+  ViewEncapsulation,
+  AfterContentChecked,
+  AfterViewChecked,
+} from "@angular/core";
+import { Course } from "../../model/course";
+
+import { CoursesService } from "../services/courses.service";
+
+@Component({
+  selector: "course-card",
+  templateUrl: "./course-card.component.html",
+  styleUrls: ["./course-card.component.css"],
+  standalone: false,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CourseCardComponent implements OnInit {
+  @Input()
+  course!: Course;
+
+  @Input()
+  cardIndex!: number;
+
+  @Output("courseChanged")
+  courseEmitter = new EventEmitter<Course>();
+
+  constructor(
+    private coursesService: CoursesService,
+    @Attribute("type") private type: string,
+  ) {}
+
+  ngOnInit() {}
+
+  onTitleChanged(newTitle: string) {
+    this.course.description = newTitle;
+  }
+
+  onSaveClicked(description: string) {
+    this.courseEmitter.emit({ ...this.course, description });
+  }
+}
